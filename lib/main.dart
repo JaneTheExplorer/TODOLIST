83aa09ad2d82c48e4e9b92c4a166885a78ef0b8d
Geoffrey Githaiga <githaiga.geoffrey@gmail.com>
2023-12-07 17:17:41 +0300

    test page added

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:testproject/loginscreen.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: LoginScreen());
+  }
+}
---
+++ lib/signupscreen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+import 'package:testproject/loginscreen.dart';
+import 'package:testproject/test.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(height: 130),
+            const Text(
+              'Create Account',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Center(
+              child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email Address',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.password),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: ' Confirm Password',
+                prefixIcon: Icon(Icons.password),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: 400,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.white),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) => const Test())));
+                },
+                child: const Text('Sign up'),
+              ),
+            ),
+            Row(
+              children: [
+                Center(
+                  child: Row(children: [
+                    const Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          foregroundColor: Colors.lightBlue[500]),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => const LoginScreen())));
+                      },
+                      child: const Center(
+                        child: Text(
+                          'Log in',
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
